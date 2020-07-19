7c605f0eb9a619fedc7793d31993a7f075fa72f8
Rahul Gupta <swastikmedical74@gmail.com>
2020-07-19 18:56:19 +0530

    Implemented Local Storage Functionality for saving Admin Palettes and User created Palettes to Local Storage

--- src/App.js
+++ src/App.js
@@ -12,16 +12,21 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props)
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"))
     this.state = {
-      palettes: seedColors
+      palettes: savedPalettes | seedColors
     }
     this.findPalette = this.findPalette.bind(this)
     this.savePalette = this.savePalette.bind(this)
+    this.syncLocalStorage = this.syncLocalStorage.bind(this)
   }
   savePalette(newPalette) {
     this.setState({
       palettes : [...this.state.palettes,newPalette]
-    })
+    }, this.syncLocalStorage)
+  }
+  syncLocalStorage() {
+    window.localStorage.setItem("palettes",JSON.stringify(this.state.palettes))
   }
   findPalette(id) {
     return this.state.palettes.find( palette => palette.id===id )
